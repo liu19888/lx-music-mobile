58e7f3c0341152c2d9c64146c40b14550fbbdd71
WIN-KN3VVTK6MKA\EDY <211977992@qq.com>
2026-05-18 14:52:28 +0800

    fix: restore react native cli platform config

---
+++ react-native.config.js
@@ -0,0 +1,25 @@
+﻿const ios = require('@react-native-community/cli-platform-ios')
+const android = require('@react-native-community/cli-platform-android')
+const communityCliPlugin = require('@react-native/community-cli-plugin/dist/index.flow.js')
+
+const commands = [
+  ...ios.commands,
+  ...android.commands,
+  communityCliPlugin.bundleCommand,
+  communityCliPlugin.ramBundleCommand,
+  communityCliPlugin.startCommand,
+].filter(Boolean)
+
+module.exports = {
+  commands,
+  platforms: {
+    ios: {
+      projectConfig: ios.projectConfig,
+      dependencyConfig: ios.dependencyConfig,
+    },
+    android: {
+      projectConfig: android.projectConfig,
+      dependencyConfig: android.dependencyConfig,
+    },
+  },
+}
